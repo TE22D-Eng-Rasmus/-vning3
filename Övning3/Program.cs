e7a70c6c69b95a22a00a2a44a04418af0ed3486e
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2024-11-26 10:22:28 +0100

    first commit

---
+++ Övning3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+static void Hello32(){
+    for (int i = 0; i <= 32; i++)
+{
+  Console.WriteLine("Hello World");
+}
+}
+Hello32();
+
+static void interger(int nummer){
+Console.WriteLine($"Kvardraten av {nummer} är {nummer*nummer}");
+}
+interger(4);
