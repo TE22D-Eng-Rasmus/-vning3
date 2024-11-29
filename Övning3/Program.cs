ae13a4a0686b7c0fb5cf067252ed91de69bfe450
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2024-11-29 12:33:01 +0100

    RightTriangleArea

--- Övning3/Program.cs
+++ Övning3/Program.cs
@@ -11,3 +11,17 @@ static void interger(int nummer){
 Console.WriteLine($"Kvardraten av {nummer} är {nummer*nummer}");
 }
 interger(4);
+
+static void multi(float nummer1, float nummer2){
+Console.WriteLine($"{nummer1} gånger {nummer2} är {nummer1*nummer2}");
+}
+
+multi(3,8);
+
+static void RightTriangleArea(float n1, float n2){
+  Console.WriteLine($"En rätvinklig triangel men basen {n1} och höjden {n2} har arean {(n1*n2)/2}");
+}
+RightTriangleArea(5,8);
+
+
+Console.ReadLine();
--- Övning3/obj/Debug/net8.0/Övning3.AssemblyInfo.cs
+++ Övning3/obj/Debug/net8.0/Övning3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Övning3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a70c6c69b95a22a00a2a44a04418af0ed3486e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Övning3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Övning3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
